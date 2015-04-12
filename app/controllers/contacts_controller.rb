60f00da496eeaca9c539afa4df1772d2da26c091
Hashmi <talha.hashmi@outlook.com>
2015-04-12 06:11:30 +0000

    Again created database table

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,16 +4,5 @@ class ContactsController < ApplicationController
   end
   
   def create  
-    @contact =Contact.new(contact_params)
-    
-    if @contact.save
-      redirect_to_new_contact_path, notice: "Messsage Sent." 
-    else
-      redirect_to_new_contact_path, notice: "Error Occured"      
   end
-    
-    private
-      def contact_params
-        params.require(:contact).permit(:name, :email, :comments)
-      end
 end
